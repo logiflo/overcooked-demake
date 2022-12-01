2fa4352ef29da82e5f8a750f712476ce611328e4
Lorena Gil <lorenagil@fb.com>
2022-12-01 09:12:52 +0000

    Added global variables to global file.

---
+++ src/classes/Globals.js
@@ -0,0 +1,37 @@
+const BarColors = {
+  Green: 0x00ff00,
+  Yellow: 0xffff00,
+  Red: 0xff0000,
+}
+
+const OrderPoints = {
+  Green: 100,
+  Yellow: 50,
+  Red: 25,
+}
+
+const TypeEntity = {
+  Box: "Box",
+  Table: "Table",
+  Fruit: "Fruit",
+  Glass: "Glass",
+  GlassFruit: "GlassFruit",
+  Kitchen: "Kitchen",
+  Delivery: "Delivery",
+  Blender: "Blender"
+};
+
+const State = {
+  Empty: "Empty",
+  Busy: "Busy",
+  Ready: "Ready",
+};
+
+const Smoothie = {
+  fruits_0: "items_6",
+  fruits_1: "items_5",
+  fruits_2: "items_4",
+  fruits_3: "items_5",
+};
+
+export { BarColors, OrderPoints, TypeEntity, State, Smoothie };

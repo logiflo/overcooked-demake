aacb3ffc1f9731c1a2a5ddc504fc955eab1cc97b
Lorena Gil <lorenagil@fb.com>
2022-12-01 12:10:30 +0000

    Added logo.

--- src/scenes/startScene.js
+++ src/scenes/startScene.js
@@ -4,11 +4,14 @@ class Start extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image("background", "./assets/overcooked-logo.webp")
     this.load.image("playBtn", "./assets/playbtn.jpg");
   }
 
   create() {
-    let startButton = this.add.image(300, 200, 'playBtn').setScale(0.2).setInteractive().on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+    const background = this.add.image(300, 200, "background");
+
+    let startButton = this.add.image(300, 300, 'playBtn').setScale(0.2).setInteractive().on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
       console.log("pressed");
       this.scene.start("game");
     });

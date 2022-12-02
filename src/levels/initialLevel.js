7bf8e201879221fd1133a4e00e6d9f544dbee2e0
Lorena Gil <lorenagil@fb.com>
2022-12-02 11:01:21 +0000

    Redesigned initialLevel.

--- src/levels/initialLevel.js
+++ src/levels/initialLevel.js
@@ -8,7 +8,7 @@ function createEntity(game, x, y, furniture) {
     game.physics,
     x,
     y,
-    "textures2",
+    "textures",
     furniture[0],
     TypeEntity[furniture[1]]
   )
@@ -21,6 +21,7 @@ function createLevel1(game, collisionSystem, audioManager) {
   //Create horizontal tables
   for (let cnt = 0; cnt < 7; cnt++) {
     let furniture = ["environment_01", "Table", null];
+    if (cnt === 0 || cnt === 6) furniture = ["environment_02", "Table", null]
     if (cnt === 3) furniture = ["environment_00", "Box", "fruits_0"];
 
     let entity = createEntity(game, (150 + cnt * entitySize.x), 100, furniture);
@@ -41,7 +42,7 @@ function createLevel1(game, collisionSystem, audioManager) {
   // Create first vertical tables
   for (let cnt = 1; cnt < 4; cnt++) {
     let furniture = ["environment_03", "Table", null];
-    if (cnt === 3) furniture = ["environment_00", "Box", "fruits_1"];
+    if (cnt === 3) furniture = ["environment_09", "Box", "fruits_1"];
 
     let entity = createEntity(game, 150, (100 + cnt * entitySize.y), furniture);
     entity.sprite.setName(furniture[2]);
@@ -55,14 +56,14 @@ function createLevel1(game, collisionSystem, audioManager) {
   // Create second vertical tables
   for (let cnt = 1; cnt < 4; cnt++) {
     let furniture = ["environment_03", "Table", null];
-    if (cnt === 2) furniture = ["environment_00", "Box", "fruits_2"];
+    if (cnt === 1) furniture = ["environment_08", "Box", "fruits_2"];
     if (cnt === 3) furniture = ["environment_10", "Kitchen", null];
 
     let entity = createEntity(game, (150 + 6 * entitySize.x), (100 + cnt * entitySize.y), furniture);
 
-    if (cnt === 2) {
-      const entity = createEntity(game, (150 + 6 * entitySize.x), (95 + cnt * entitySize.y), ["fruits_2" , "Box"]);
-      collisionSystem.addEntity(entity);
+    if (cnt === 1) {
+      entity.sprite.setName(furniture[2]);
+      createEntity(game, (150 + 6 * entitySize.x), (100 + cnt * entitySize.y), ["fruits_2" , "Box"]);
     }
 
     if (cnt === 3) {
@@ -70,7 +71,7 @@ function createLevel1(game, collisionSystem, audioManager) {
         game,
         150 + 6 * entitySize.x,
         95 + cnt * entitySize.y,
-        "textures2",
+        "textures",
         furniture[0],
         furniture[1],
         true,
